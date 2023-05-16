52bccbe4dd916c395987578b9c0e41397eac6eac
Jacob Poirier <jacobapoirier9@gmail.com>
2023-05-16 12:34:20 -0400

    add test files

---
+++ CommandSurfacer.Tests/ArgsParserTests.cs
@@ -0,0 +1,14 @@
+﻿using CommandSurfacer.Services;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CommandSurfacer.Tests;
+
+public class ArgsParserTests : BaseTests
+{
+    private readonly IArgsParser _argsParser;
+
+    public ArgsParserTests(CommandSurfacerTestFixture fixture)
+    {
+        _argsParser = fixture.AppHost.Services.GetRequiredService<IArgsParser>();
+    }
+}
---
+++ CommandSurfacer.Tests/CommandSurfacerTestFixture.cs
@@ -0,0 +1,32 @@
+﻿using CommandSurfacer.Services;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System.Reflection;
+
+namespace CommandSurfacer.Tests;
+
+public class CommandSurfacerTestFixture : IDisposable
+{
+    private static string AssemblyExePath => Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location) ?? Environment.CurrentDirectory;
+
+    private readonly IHost _appHost;
+    public IHost AppHost => _appHost;
+
+    public CommandSurfacerTestFixture()
+    {
+        _appHost = Host.CreateDefaultBuilder()
+            .UseContentRoot(AssemblyExePath)
+            .ConfigureServices(services =>
+            {
+                var commandSurfaces = new List<CommandSurface>();
+
+                services.AddSingleton(commandSurfaces);
+                services.AddSingleton<IStringConverter, StringConverter>();
+                services.AddSingleton<IArgsParser, ArgsParser>();
+                services.AddSingleton<ICommandRunner, CommandRunner>();
+            })
+            .Build();
+    }
+
+    public void Dispose() => _appHost?.Dispose();
+}
