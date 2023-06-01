9cb7ddff52e23020205e72e9a5da5b810ae6715d
Jacob Poirier <jacobapoirier9@gmail.com>
2023-06-01 15:43:23 -0400

    add bool back

--- CommandSurfacer/Services/StringConverter.cs
+++ CommandSurfacer/Services/StringConverter.cs
@@ -6,8 +6,32 @@ public class StringConverter : IStringConverter
 
     public StringConverter()
     {
+        var allowedTrueValues = new string[] { "true", "yes", "y", "1" };
+        var allowedFalseValues = new string[] { "false", "no", "n", "0" };
+
         _converters = new Dictionary<Type, Func<string, object>>()
         {
+            {   typeof(bool), (input) =>
+                {
+                    if (allowedTrueValues.Contains(input, StringComparer.OrdinalIgnoreCase))
+                        return true;
+                    else if (allowedFalseValues.Contains(input, StringComparer.OrdinalIgnoreCase))
+                        return false;
+
+                    return Activator.CreateInstance<bool>();
+                }
+            },
+            {
+                typeof(bool?), (input) =>
+                {
+                    if (allowedTrueValues.Contains(input, StringComparer.OrdinalIgnoreCase))
+                        return true;
+                    else if (allowedFalseValues.Contains(input, StringComparer.OrdinalIgnoreCase))
+                        return false;
+
+                    return Activator.CreateInstance<bool?>();
+                }
+            },
             { typeof(string), (input) => input },
             { typeof(byte), (input) => byte.Parse(input) },
             { typeof(byte?), (input) => byte.TryParse(input, out var outValue) ? outValue : null },
