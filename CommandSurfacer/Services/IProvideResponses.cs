4dbe9093bb113dd769c0b3febb9efedd2cfaf9ed
Jacob Poirier <jacobapoirier9@gmail.com>
2023-06-23 18:07:08 -0400

    delete unused files

--- CommandSurfacer/Services/IProvideResponses.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace CommandSurfacer.Services;
-
-public interface IProvideResponses
-{
-    public T GetResponse<T>(string prompt);
-    public object GetResponse(string prompt, Type targetType);
-    public string GetResponse(string prompt);
-}
